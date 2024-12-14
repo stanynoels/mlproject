aa8d08ea9d4cffff29ed50f74744f2ec1bd9091f
noel <stanynoel4@gmail.com>
2024-12-14 12:52:36 +0530

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requiremnts = []
+    with open(file_path) as file:
+        requiremnts = file.readlines()
+        requiremnts = [req.replace("\n", "") for req in requiremnts]
+
+        if HYPHEN_E_DOT in requiremnts:
+            requiremnts.remove(HYPHEN_E_DOT)
+
+    return requiremnts
+
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Noel Stany',
+author_email='stanynoel4@gmail.com',
+py_modules=get_requirements("requirements.txt"),
+)
